ea581272aa9c4cdf6cce8ed85c238b540cc377c2
ansgdv <mysantema@gmail.com>
2024-12-09 20:49:50 +0300

    hw 2

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -30,7 +30,9 @@ namespace Model.Runtime.Projectiles
             // Insert you code here
             ///////////////////////////////////////
             float maxHeight = totalDistance * 0.6f;
-            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
+            float timeFactor = t * 2 - 1;
+
+            localHeight = maxHeight * (- timeFactor * timeFactor + 1);
 
             ///////////////////////////////////////
             // End of the code to insert
