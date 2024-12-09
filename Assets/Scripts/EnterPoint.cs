afcfd34963c23b2c6135118f868e8c03c5453fa1
ansgdv <mysantema@gmail.com>
2024-12-09 19:28:53 +0300

    hw 1
    Change timeScale to 5

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,7 +12,6 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 5;
 
-
     void Start()
     {
         Time.timeScale = _timeScale;
