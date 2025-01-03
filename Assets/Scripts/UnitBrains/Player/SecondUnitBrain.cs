801446108fdbac56130d186896bd7e2d0d7491f9
ansgdv <mysantema@gmail.com>
2025-01-03 21:15:41 +0300

    hw-4

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Model.Runtime.Projectiles;
 using UnityEngine;
 
@@ -32,16 +33,6 @@ namespace UnitBrains.Player
             }
             IncreaseTemperature();
 
-            //if (GetTemperature() < overheatTemperature)
-            //{
-            //    for (int i = 0; i <= GetTemperature(); i++)
-            //    {
-            //        var projectile = CreateProjectile(forTarget);
-            //        AddProjectileToList(projectile, intoList);
-            //    }   
-            //    IncreaseTemperature();
-            //}
-
             ///////////////////////////////////////
         }
 
@@ -56,10 +47,32 @@ namespace UnitBrains.Player
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
             List<Vector2Int> result = GetReachableTargets();
+            
+            if (!result.Any())
+            {
+                return result;
+            }
+
+            Vector2Int nearestTarget = result[0];
+            float minDistance = DistanceToOwnBase(nearestTarget);
+
+            foreach (var target in result)
+            {
+                float distance = DistanceToOwnBase(target);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestTarget = target;
+                }
+            }
+
+            result[0] = nearestTarget;
+
             while (result.Count > 1)
             {
                 result.RemoveAt(result.Count - 1);
             }
+
             return result;
             ///////////////////////////////////////
         }
